33b0b6ea3c3bb9de1e0c1e828102cec67616d23e
Jaid Monwar Chowdhury <jaidmonwar.official@gmail.com>
2023-01-27 08:10:18 -0800

    1st Commit

---
+++ blockchain/block.js
@@ -0,0 +1,68 @@
+const hexToBinary = require("hex-to-binary");
+const { GENESIS_DATA, MINE_RATE } = require("../config");
+const { cryptoHash } = require("../util");
+
+class Block {
+  constructor({ timestamp, lastHash, hash, data, nonce, difficulty }) {
+    // {arguments} - helps to not follow order of arguments
+    this.timestamp = timestamp;
+    this.lastHash = lastHash;
+    this.hash = hash;
+    this.data = data;
+    this.nonce = nonce;
+    this.difficulty = difficulty;
+  }
+
+  static genesis() {
+    return new this(GENESIS_DATA);
+  }
+
+  static mineBlock({ lastBlock, data }) {
+    const lastHash = lastBlock.hash;
+    let hash, timestamp;
+    let { difficulty } = lastBlock;
+    let nonce = 0;
+
+    do {
+      nonce++;
+      timestamp = Date.now();
+      difficulty = Block.adjustDifficulty({
+        originalBlock: lastBlock,
+        timestamp,
+      });
+      hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
+    } while (
+      hexToBinary(hash).substring(0, difficulty) !== "0".repeat(difficulty)
+    );
+
+    return new this({
+      timestamp,
+      lastHash,
+      data,
+      difficulty,
+      nonce,
+      hash,
+    });
+  }
+
+  static adjustDifficulty({ originalBlock, timestamp }) {
+    const { difficulty } = originalBlock;
+
+    if (difficulty < 1) return 1;
+
+    if (timestamp - originalBlock.timestamp > MINE_RATE) return difficulty - 1;
+
+    return difficulty + 1;
+  }
+}
+
+// const block1 = new Block({
+//     timestamp: '01/01/01',
+//     lastHash: 'foo-lastHash',
+//     hash: 'foo-hash',
+//     data: 'foo-data'
+// }); //Making an Instance of class Block
+
+// console.log('block1', block1);
+
+module.exports = Block;
